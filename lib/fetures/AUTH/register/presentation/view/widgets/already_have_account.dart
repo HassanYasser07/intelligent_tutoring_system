fe6eed9bfc96fd2d252ff7cdc8ceeaf32c161615
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-04-13 10:24:33 +0200

    add  AlreadyHaveAccountText widget

---
+++ lib/fetures/AUTH/register/presentation/view/widgets/already_have_account.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:intelligent_tutoring_system/core/theme/style.dart';
+
+
+class AlreadyHaveAccountText extends StatelessWidget {
+  final String text;
+  final VoidCallback onActionPressed;
+  
+  const AlreadyHaveAccountText({
+    super.key,
+    required this.text,
+    required this.onActionPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Already have an account?',
+            style: Styles.font13DarkBlueRegular,
+          ),
+          TextSpan(
+            text: ' $text',
+            style: Styles.font13BlueSemiBold,
+            recognizer: TapGestureRecognizer()
+              ..onTap = onActionPressed,
+          ),
+        ],
+      ),
+    );
+  }
+}
