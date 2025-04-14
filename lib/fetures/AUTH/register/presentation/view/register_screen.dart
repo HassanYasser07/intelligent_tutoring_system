d0acf37ab452753e7de3b881a0e82f2f231dc54b
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-04-14 11:20:49 +0200

    register feature done

--- lib/fetures/AUTH/register/presentation/view/register_screen.dart
+++ lib/fetures/AUTH/register/presentation/view/register_screen.dart
@@ -5,6 +5,7 @@ import 'package:intelligent_tutoring_system/core/general%20widgets/app_text_butt
 import 'package:intelligent_tutoring_system/core/routing.dart';
 import 'package:intelligent_tutoring_system/core/theme/style.dart';
 import 'package:intelligent_tutoring_system/fetures/AUTH/register/presentation/view/widgets/already_have_account.dart';
+import 'package:intelligent_tutoring_system/fetures/AUTH/register/presentation/view/widgets/dialogs.dart';
 import 'package:intelligent_tutoring_system/fetures/AUTH/register/presentation/view/widgets/sign_up_form.dart';
 import '../cubit/register_cubit.dart';
 import '../cubit/register_state.dart';
@@ -68,66 +69,4 @@ class RegisterScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-void showSuccessDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    barrierDismissible: false, // Prevent dismissing by tapping outside
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: const Text('Signup Successful'),
-        content: const SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              Text('Congratulations, you have signed up successfully!'),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.blue,
-              disabledForegroundColor: Colors.grey.withOpacity(0.38),
-            ),
-            onPressed: () {
-              Navigator.of(context).pop(); // Close the dialog
-              GoRouter.of(context)
-                  .push(Routes.kCourseView); // Navigate to login
-            },
-            child: const Text('Continue'),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-void setupErrorState(BuildContext context, String error) {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      icon: const Icon(
-        Icons.error,
-        color: Colors.red,
-        size: 32,
-      ),
-      content: Text(
-        error,
-        style: Styles.font15DarkBlueMedium,
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          }, // Just close the dialog
-          child: Text(
-            'Got it',
-            style: Styles.font14BlueSemiBold,
-          ),
-        ),
-      ],
-    ),
-  );
 }
