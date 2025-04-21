15c2130dbfa974d78115c4f1e2f0e671c9893fc8
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-04-22 00:13:54 +0200

    create splash view

---
+++ lib/fetures/spalsh/presentation/views/widgets/Splash_view_body.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intelligent_tutoring_system/core/helper/token_storage.dart';
+import 'package:intelligent_tutoring_system/fetures/spalsh/presentation/views/widgets/slidind_text.dart';
+import '../../../../../core/routing.dart';
+
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  static const kTransitionDuration = Duration(milliseconds: 1500);
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+   navigateToHome();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset('assets/duolingo.gif',width: 100,height: 100,),
+       SlidingText(slidingAnimation: slidingAnimation)
+
+      ],
+    );
+  }
+  void initSlidingAnimation() {
+    animationController=AnimationController(
+        vsync: this,
+        duration: kTransitionDuration);
+    slidingAnimation =Tween<Offset>(begin:const Offset(0,3) ,end:const Offset(0,0) ).animate(animationController);
+    animationController.forward();
+  }
+  void navigateToHome() 
+  async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (!mounted) return;
+    
+    final token = await AppStorage.getToken();
+    if (token == null) {
+      context.go(Routes.kWelcomeScreen);
+    } else {
+      context.go(Routes.kCourseView);
+    }
+  }
+}
+
+
