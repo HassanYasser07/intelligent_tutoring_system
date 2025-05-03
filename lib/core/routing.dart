55370f5426a7c123b382179e05db549023d74ea5
Hassan Yasser <108157502+HassanYasser07@users.noreply.github.com>
2025-05-03 10:15:18 +0300

    create topics view ui done store user path in shared preferences

--- lib/core/routing.dart
+++ lib/core/routing.dart
@@ -1,8 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intelligent_tutoring_system/core/networking/token_service.dart';
 import 'package:intelligent_tutoring_system/fetures/AUTH/login/data/loginRepo/login_repo.dart';
@@ -10,33 +9,36 @@ import 'package:intelligent_tutoring_system/fetures/AUTH/login/presentation/cubi
 import 'package:intelligent_tutoring_system/fetures/AUTH/register/data/repository/register_repository.dart';
 import 'package:intelligent_tutoring_system/fetures/AUTH/register/presentation/cubit/register_cubit.dart';
 import 'package:intelligent_tutoring_system/fetures/AUTH/register/presentation/view/register_screen.dart';
-import 'package:intelligent_tutoring_system/fetures/Getting%20learning%20goal%20&%20knowledge%20base/data/models/goal_and%20_knowledge_base_response_model.dart';
-import 'package:intelligent_tutoring_system/fetures/Getting%20learning%20goal%20&%20knowledge%20base/data/repo/goal_and%20_knowledge_base_repo.dart'
-    show GoalAndKnowledgeBaseRepo;
-import 'package:intelligent_tutoring_system/fetures/Getting%20learning%20goal%20&%20knowledge%20base/presentation/cubit/shat_boot_cubit.dart'
-    show LearningAnalysisCubit;
-import 'package:intelligent_tutoring_system/fetures/Getting%20learning%20goal%20&%20knowledge%20base/presentation/view/shat_boot_view.dart';
 import 'package:intelligent_tutoring_system/fetures/Los/vedio/los_view.dart';
-import 'package:intelligent_tutoring_system/fetures/courses/presentation/course_view.dart';
+import 'package:intelligent_tutoring_system/fetures/learning_style_questions/presentation/result_view.dart';
 import 'package:intelligent_tutoring_system/fetures/learning_style_questions/presentation/widgets/question_widgits.dart';
-import 'package:intelligent_tutoring_system/fetures/lessons/presentation/lesson_view.dart';
 import 'package:intelligent_tutoring_system/fetures/presentingLos/data/api_services/los_api_services.dart';
 import 'package:intelligent_tutoring_system/fetures/presentingLos/data/repo/los_repo.dart';
 import 'package:intelligent_tutoring_system/fetures/presentingLos/presentation/cubit/los_cubit.dart';
 import 'package:intelligent_tutoring_system/fetures/presentingLos/presentation/view/los_view.dart';
+import 'package:intelligent_tutoring_system/fetures/profile/data/repo/profile_repo.dart';
+import 'package:intelligent_tutoring_system/fetures/profile/presentation/cubit/profile_cubit.dart';
+import 'package:intelligent_tutoring_system/fetures/profile/presentation/view/profile%20view.dart';
 import 'package:intelligent_tutoring_system/fetures/spalsh/presentation/views/splash_view.dart';
-import 'package:intelligent_tutoring_system/fetures/topics/data/api_services/user_knowledge_api_services.dart'
-    show UserKnowledgeApiServices;
-import 'package:intelligent_tutoring_system/fetures/topics/data/repo/user_knowledge_repo.dart';
-import 'package:intelligent_tutoring_system/fetures/topics/presentation/cubit/user_knowledge_cubit.dart'
-    show UserKnowledgeCubit;
-import 'package:intelligent_tutoring_system/fetures/topics/presentation/widgets/series_of_topics.dart';
-import 'package:intelligent_tutoring_system/fetures/topics/topics_view.dart';
+import 'package:intelligent_tutoring_system/fetures/topics/data/api_services/user_knowledge_api_services.dart' show UserKnowledgeApiServices;
+import 'package:intelligent_tutoring_system/fetures/topics/presentation/cubit/user_knowledge_cubit.dart' show UserKnowledgeCubit;
+import 'package:intelligent_tutoring_system/fetures/topics/presentation/series_of_topics.dart';
 import 'package:intelligent_tutoring_system/fetures/welcome/presentation/view/welcome_screen.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../fetures/AUTH/login/presentation/login_view.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/data/api_services/api_services.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/data/models/goal_and_knowledge_base_response_model.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/data/repo/goal_and _knowledge_base_repo.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/presentation/cubit/shat_boot_cubit.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/presentation/view/presenting_base_goal.dart';
+import '../fetures/Getting_learning_goal_knowledge_base/presentation/view/shat_boot_view.dart';
+import '../fetures/profile/data/apiServices/profile_api_services.dart';
+import '../fetures/success/presentation/course_view.dart';
+import 'helper/shatboot_response_storage.dart';
 import 'helper/token_storage.dart';
-import 'package:intelligent_tutoring_system/fetures/Getting%20learning%20goal%20&%20knowledge%20base/data/api%20services/api_services.dart';
+
+
 
 /// The route configuration.
 
@@ -50,6 +52,9 @@ class Routes {
   static const kLearningStyleQuestionsView = '/LearningStyleQuestionsView';
   static const kShatBootView = '/ShatBootView';
   static const kWelcomeScreen = '/welcomeScreen';
+  static const kResultView = '/result';
+  static const kPresentingBaseGoalView = '/presentingBaseGoalView';
+  static const kProfileView = '/profileView';
 
   static final GoRouter router = GoRouter(
     initialLocation: '/',
@@ -67,8 +72,17 @@ class Routes {
                 // Verify token
                 final tokenService = TokenService();
                 await tokenService.verifyToken(token);
+
+               //  Check for saved LearningAnalysisResponse
+               // final prefs = await SharedPreferences.getInstance();
+                final prefsService = SharedPreferencesService();
+                final savedResponse = await  prefsService.getLearningAnalysisResponse();
                 // Token is valid
-                return context.go(kCourseView);
+                if (savedResponse != null) {
+                  return context.go(Routes.kTopicsView, extra: savedResponse);
+                } else {
+                  return context.go(Routes.kCourseView);
+                }
               } on UnauthorizedException {
                 // Token is invalid or expired
                 await AppStorage.deleteToken(); // Clear invalid token
@@ -105,7 +119,7 @@ class Routes {
         builder: (BuildContext context, GoRouterState state) {
           return BlocProvider(
             create: (context) => RegisterCubit(RegisterRepository(Dio())),
-            child: RegisterScreen(),
+            child: const RegisterScreen(),
           );
         },
       ),
@@ -148,19 +162,9 @@ class Routes {
       GoRoute(
         path: kCourseView,
         builder: (BuildContext context, GoRouterState state) {
-          return const CourseView();
+          return const SuccessScreen();
         },
       ),
-
-//       GoRoute(
-//         path: kTopicsView,
-//         builder: (BuildContext context, GoRouterState state) {
-//           return  BlocProvider(
-//   create: (context) => UserKnowledgeCubit(UserKnowledgeApiServices(Dio()), SharedPreferences()),
-//   child:  UserPathScreen(analysisResponse: LearningAnalysisResponse(knowledgeBase: [], learningGoal: [])),
-// );
-//         },
-//       ),
       GoRoute(
         path: kLosView,
         builder: (BuildContext context, GoRouterState state) {
@@ -179,6 +183,43 @@ class Routes {
           return const QuestionWidget();
         },
       ),
+      GoRoute(
+        path: kPresentingBaseGoalView,
+        builder: (BuildContext context, GoRouterState state) {
+          return  PresentingBaseGoalView(response: state.extra as LearningAnalysisResponse,);
+        },
+      ),
+      GoRoute(
+        path: kResultView,
+        builder: (BuildContext context, GoRouterState state) {
+          final results = state.extra as Map<String, dynamic>;
+          return ResultView(results: results);
+        },
+      ),
+    GoRoute(
+  path: kProfileView,
+  builder: (context, state) {
+    return FutureBuilder(
+      future: AppStorage.getToken(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Scaffold(body: Center(child: Text("Not logged in")));
+        }
+        final email = JwtDecoder.decode(snapshot.data!)['email'];
+        return BlocProvider(
+          create: (_) => ProfileCubit(
+            ProfileRepo(
+              ProfileApiService(Dio()),
+            ),
+          )..getProfile(email),
+          child: const ProfileScreen(),
+        );
+      },
+    );
+  },
+),
+
+
     ],
   );
 }
